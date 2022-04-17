1181fe527c4136f052db3c9c44cc15b71c3a13fc
Renad <renad.gharz@outlook.com>
2022-04-17 13:22:52 -0400

    Pulled rankings data for all divisions except for #1 P4P and champions into Pandas dataframe

--- rankings.py
+++ rankings.py
@@ -1,1 +1,6 @@
+import requests, bs4, pandas as pd, pprint as pp
 
+df_list = pd.read_html('https://www.ufc.com/rankings')
+
+p4p, fly, bantam, feather, light, welter, middle, lheavy, heavy = df_list[0:9] #men's divisions including P4P
+w_p4p, w_straw, w_fly, w_bantam, w_feather = df_list[10:13] #women's division including P4P
