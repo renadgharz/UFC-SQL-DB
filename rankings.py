86cb32dac48bb3e3d48046788869241e31e44534
Renad <renad.gharz@outlook.com>
2022-04-18 13:08:46 -0400

    Created rankings_data() function

--- rankings.py
+++ rankings.py
@@ -1,6 +1,20 @@
 import requests, bs4, pandas as pd, pprint as pp
 
-df_list = pd.read_html('https://www.ufc.com/rankings')
+url = 'https://www.ufc.com/rankings'
+resp = requests.get(url)
+soup = bs4.BeautifulSoup(resp.text, 'html.parser')
+df_list = pd.read_html(url)
 
-p4p, fly, bantam, feather, light, welter, middle, lheavy, heavy = df_list[0:9] #men's divisions including P4P
-w_p4p, w_straw, w_fly, w_bantam, w_feather = df_list[10:13] #women's division including P4P
+def rankings_data():
+
+
+    for i in df_list:
+        i.columns = ['Rank', 'Fighter', 'Change']
+        i.fillna('', inplace= True)
+
+    p4p, fly, bantam, feather, light, welter, middle, lheavy, heavy = df_list[0:9] #men's divisions including P4P
+    w_p4p, w_straw, w_fly, w_bantam= df_list[9:13] #women's division including P4P, excluding Women's Featherweight because there are no contenders
+
+    pp.pprint(tags)
+
+rankings_data()
