65402ca1bcbbf87d7b4478a31771f854e39d69ba
Renad <renad.gharz@outlook.com>
2022-04-21 23:09:43 -0400

    Added division champions/#1's

--- rankings.py
+++ rankings.py
@@ -7,7 +7,6 @@ df_list = pd.read_html(url)
 
 def rankings_data():
 
-
     for i in df_list:
         i.columns = ['Rank', 'Fighter', 'Change']
         i.fillna('', inplace= True)
@@ -15,6 +14,10 @@ def rankings_data():
     p4p, fly, bantam, feather, light, welter, middle, lheavy, heavy = df_list[0:9] #men's divisions including P4P
     w_p4p, w_straw, w_fly, w_bantam= df_list[9:13] #women's division including P4P, excluding Women's Featherweight because there are no contenders
 
-    pp.pprint(tags)
+    # Work in progress
+    for champ in soup.select('div.rankings--athlete--champion')[0:13]:
+        x = map(champ.h5.text,i)
+
+        pp.pprint(x)
 
 rankings_data()
