600a8a535491b8829e401a5b62f12d9b4be03bf7
Renad <renad.gharz@outlook.com>
2022-05-24 19:11:40 -0600

    Events page load

--- events.py
+++ events.py
@@ -1,1 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import bs4
+import requests
 
+url = 'https://www.ufc.com/events#events-list-past/'
+
+resp = requests.get(url)
+soup = bs4.BeautifulSoup(resp.content, 'html.parser')
+
+driver = webdriver.Edge()
+driver.get(url)
+driver.implicitly_wait(0.5)
+
+event_cards = driver.find_elements(by=By.CLASS_NAME, value='l-listing__item')
+
+for event_card in event_cards:
+    event_link = driver.find_elements(by=By.XPATH, value='//*[@id="events-list-past"]/div/div/div[2]/div/div/section/ul/li[1]/article/div[1]/div/a')
+
+
+driver.find_element(
+    by=By.XPATH,
+    value='//*[@id="events-list-past"]/div/div/ul/li/a').\
+    send_keys("webdriver" + Keys.ENTER)
+
+
+# driver.quit()
--- rankings.py
+++ rankings.py
@@ -6,11 +6,10 @@ url = 'https://www.ufc.com/rankings'
 resp = requests.get(url)
 soup = bs4.BeautifulSoup(resp.content, 'html.parser')
 
-divisions = pd.read_html(url)
-
 
 def get_rankings():
 
+    divisions = pd.read_html(url)
     for division in divisions:
         division.columns = ['Rank', 'Fighter', 'Change']
         division.fillna('', inplace=True)
@@ -18,17 +17,20 @@ def get_rankings():
     champions = soup.select('table.cols-0')[0:13]
     for champ, division in zip(champions, divisions):
         try:
-            division.loc[-1] = ['Champion/#1', champ.h5.text.strip("\n"), ' ']
+            division.loc[-1] = ['Champion', champ.h5.text.strip("\n"), ' ']
             division.index = division.index + 1
             division.sort_index(inplace=True)
 
         except:
-            division.loc[-1] = ['Champion/#1', 'VACANT TITLE', ' ']
+            division.loc[-1] = ['Champion', 'Vacant', ' ']
             division.index = division.index + 1
             division.sort_index(inplace=True)
 
     p4p, fly, bantam, feather, light, welter, middle, lheavy, heavy = divisions[0:9]  # men's divisions including P4P
     w_p4p, w_straw, w_fly, w_bantam = divisions[9:13]  # women's division including P4P, excluding Women's Featherweight
+
+    p4p = p4p.replace('Champion', '1')
+    w_p4p = w_p4p.replace('Champion', '1')
 
 
 get_rankings()
